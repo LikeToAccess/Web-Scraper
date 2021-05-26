f5a9cab327cf166b52649718488313191b0e3a0d
LTA <37591675+LikeToAccess@users.noreply.github.com>
2021-05-26 05:50:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-URL = "https://gomovies-online.cam/all-films-2"
-headers = {
-	"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
-}
-page = requests.get(URL, headers=headers)
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
-for link in soup.find_all('img'):
-	link = link.get('data-src')
-	if link:
-		if link[:44] == "https://static.gomovies-online.cam/dist/img/":
-			print(link)
+
+class Scraper:
+	def __init__(self, link):
+		self.link = link
+		options = Options()
+		options.add_argument("--headless")
+		executable = "chromedriver.exe" if os.name == "nt" else "chromedriver"
+		print(executable)
+		self.driver = webdriver.Chrome(executable_path=os.path.abspath(executable), options=options)
+		self.driver.get(self.link)
+
+	def run(self, xpath="//*[@id=\"_skqeqEJBSrS\"]/div[2]/video", attr="src"):
+		element = WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.XPATH,xpath)))
+		element = self.driver.find_element_by_xpath(xpath)
+		data = element.get_attribute(attr)
+		while len(data) < 100:
+			print("DEBUG: No data!")
+			self.driver.refresh()
+			data = self.run(xpath, attr)
+		self.driver.quit()
+		return data
+
+
+if __name__ == "__main__":
+	link = "https://gomovies-online.cam/watch-film/wrath-of-man/CCmkfFgE/kp6jEoRd"
+	scraper = Scraper(link)
+	print(scraper.run())
